41a59ca0959120d56d2a8ffbe6f2b37cdae12f96
Bo <peterbozhou@gmail.com>
2019-12-11 11:21:08 -0500

    Changes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,6 +4,7 @@ import {parsePost} from "../middlewares/parse_post";
 import {parseDelete} from "../middlewares/parse_delete";
 import {authenticateUser} from "../middlewares/auth";
 import {modifyUserPath} from "../middlewares/modify_user_path";
+const fetch = require("node-fetch");
 
 export const router = express.Router();
 export const prefix = '/user';
@@ -16,10 +17,13 @@ const {userStore} = require('../data/DataStore');
  */
 router.use([authenticateUser, modifyUserPath]);
 
-router.get('/*', parseGet, function (req, res) {
+router.get('/callback?code=:code&state=:state', parseGet, function (req, res) {
+  console.log("visited");
+  console.log(req);
+  const code = req.params.code;
   const result = req.handleGet(userStore);
   if (typeof result !== 'undefined') {
-    res.send({result})
+    res.send({'Token': code})
   }
 });
 
