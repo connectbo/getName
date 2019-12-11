53d93b7c5726a737efd4c8d1911127de720e776c
Bo <peterbozhou@gmail.com>
2019-12-10 19:06:53 -0500

    Like Function Done

--- backend/routes/public.js
+++ backend/routes/public.js
@@ -8,8 +8,17 @@ export const prefix = '/public';
 
 const {publicStore} = require('../data/DataStore');
 
+router.post('/like', parsePost, function(req,res){
+  const _id = req.body.data.id;
+  const result = req.handlePost(publicStore);
+  let likes = publicStore['_data']['playlists']['likes'];
+  let pre_likes = likes[_id]+1;
+  publicStore.set(`playlists.likes.${_id}`,pre_likes);
+  res.send(`${_id}: ${pre_likes}`);
+})
 
 router.get('/', parseGet, function (req, res) {
+  console.log("Visited");
   const result = req.handleGet(publicStore);
   if (typeof result !== 'undefined') {
     res.send(publicStore['_data'])
