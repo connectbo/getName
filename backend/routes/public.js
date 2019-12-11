85e15d9534dd228f6bd1d4f31f3eda9452c445f9
Bo <peterbozhou@gmail.com>
2019-12-10 21:39:14 -0500

    Unlike Function Done

--- backend/routes/public.js
+++ backend/routes/public.js
@@ -8,6 +8,15 @@ export const prefix = '/public';
 
 const {publicStore} = require('../data/DataStore');
 
+router.post('/unlike', parsePost, function(req,res){
+  const _id = req.body.data.id;
+  const result = req.handlePost(publicStore);
+  let likes = publicStore['_data']['playlists']['likes'];
+  let pre_likes = likes[_id]-1;
+  publicStore.set(`playlists.likes.${_id}`,pre_likes);
+  res.send(`${_id}: ${pre_likes}`);
+})
+
 router.post('/like', parsePost, function(req,res){
   const _id = req.body.data.id;
   const result = req.handlePost(publicStore);
